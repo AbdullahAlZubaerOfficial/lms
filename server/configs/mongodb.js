aee236ad39d1131514e9bc6d3108c77e97c567ce
Abdullah3002 <zubaerislam703@gmail.com>
2025-02-23 17:23:55 +0600

    first

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 // Connect to the MongoDB database
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/lms`, {
+        await mongoose.connect(`${process.env.MONGODB_URI}/ll`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
